82adaee39b973534274016eae6874837aab71899
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2020-10-30 00:15:30 +0530

    More Refactoring to multimodule project. 3.21

--- pet-clinic-data/src/test/java/com/aditya/springpetclinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-data/src/test/java/com/aditya/springpetclinic/SpringPetClinicApplicationTests.java
@@ -1,9 +1,11 @@
-package springpetclinic;
+package com.aditya.springpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootApplication
 class SpringPetClinicApplicationTests {
 
     @Test
