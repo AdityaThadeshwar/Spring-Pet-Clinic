47daba74964cb6d6023980f730ff13d3bb564355
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2020-10-30 16:23:59 +0530

    4.10: Refactoring interfaces and implementing using abstract class

--- pet-clinic-data/src/main/java/com/adiya/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/adiya/springpetclinic/services/VetService.java
@@ -2,11 +2,6 @@ package com.adiya.springpetclinic.services;
 
 import com.adiya.springpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAVet();
 }
