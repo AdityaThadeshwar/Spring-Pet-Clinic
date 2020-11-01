7203a0984b4f1f54ec68b26ea90e5a8e0bafb849
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2020-11-01 12:59:58 +0530

    5.15: Added index page data using thymeleaf to list vets/owners on webpage

--- pet-clinic-web/src/main/java/com/aditya/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/aditya/springpetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package com.aditya.springpetclinic.controllers;
 
+import com.adiya.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    public final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index.html","/vets/index"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
